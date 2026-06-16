e99b452dc14eeacbca3d3dd0a39140f84ccedb62
imsrl <iamsorealaptem@gmail.com>
2026-06-16 22:49:50 +0700

    Новое учебное приложение: баг-трекер Mantis

---
+++ fixture/application.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+
+class Application:
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.vars = {}
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        # дома поменять обратно
+        self.wd.get(self.base_url)
+        self.wd.set_window_size(1006, 892)
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/session.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, username, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def is_logged_in(self):
+        return len(self.app.wd.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        return self.get_logged_user() == username
+
+    def get_logged_user (self):
+        return self.app.wd.find_element(By.CSS_SELECTOR, "td.login-info-left span").text
+
+    def login(self, username, password):
+        self.app.open_home_page()
+        self.app.wd.find_element(By.NAME, "username").click()
+        self.app.wd.find_element(By.NAME, "username").send_keys(username)
+        self.app.wd.find_element(By.NAME, "password").send_keys(password)
+        self.app.wd.find_element(By.CSS_SELECTOR, ".button").click()
+
+    def logout(self):
+        self.app.wd.find_element(By.LINK_TEXT, "Logout").click()
+        #self.app.wd.find_element(By.CSS_SELECTOR, "html").click()
